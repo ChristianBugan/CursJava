bec47e4a8776e654b11f3b4104e6697131ceb4e0
unknown <christian.bugan@gmail.com>
2023-05-08 16:33:37 +0300

    curs1

---
+++ src/curs1/FirstClass.java
@@ -0,0 +1,26 @@
+package curs1;
+
+public class FirstClass {
+
+	//one line comment .Pot sa pun aici orice.
+	//public -> este un access modifier || String -> este un DataType || Nume -> numele variabilei
+	public String name = "Oana"; // variabile de instanta
+	public int age;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+String prenume = "Ion"; // variabila locala
+
+FirstClass obj = new FirstClass();
+System.out.println(obj.name);	
+
+System.out.println(prenume);		
+
+System.out.println("Test de luni");
+
+
+	}
+
+}
