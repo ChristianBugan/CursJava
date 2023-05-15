dcef247581a85e056a9261c21ed0b03cf75af572
unknown <christian.bugan@gmail.com>
2023-05-15 10:21:26 +0300

    curs3

---
+++ src/curs3/ParSauImpar.java
@@ -0,0 +1,36 @@
+package curs3;
+
+import java.util.Scanner;
+
+public class ParSauImpar {
+	
+	int number;
+	
+	public void askTheUser() {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter a number: ");
+		number = scan.nextInt();
+		scan.close();
+			
+	}
+	
+	// = operator de atribuire    x = 8 (ii atribui lui x valoare 8)
+	// == operator de comparatie  x == 8 (verific daca x are valoarea 8)
+	
+	public void checkNrIsOddOrEven() {
+		
+		if(number % 2 == 0) {
+			
+			System.out.println(number + ": este par!");
+			
+		}else {
+			System.out.println(number + ": este impar!");
+			
+		}
+		
+	}
+	
+	
+	
+}
---
+++ src/curs3/StaticVsNonStatic.java
@@ -0,0 +1,41 @@
+package curs3;
+
+public class StaticVsNonStatic {
+
+	//variabila statica
+	public static String nume;
+	public static String departament = "QA";
+	//variabile non-statica(variabila de instanta)
+	
+	public String prenume = "Christian";
+	
+	public static void main(String[] args) {
+	
+		/*
+		System.out.println(StaticVsNonStatic.nume);
+		
+		StaticVsNonStatic obj = new StaticVsNonStatic();
+		System.out.println(obj.prenume); */
+
+		StaticVsNonStatic person = new StaticVsNonStatic();
+		person.nume = "Oana";
+		System.out.println(person.nume);
+		System.out.println(person.departament);
+		System.out.println("---------------------");
+		StaticVsNonStatic person2 = new StaticVsNonStatic();
+		person2.nume = "Mario";
+		System.out.println(person2.nume);
+		System.out.println(person2.departament);
+		System.out.println("---------------------");
+		StaticVsNonStatic person3 = new StaticVsNonStatic();
+		person2.nume = "Trei";
+		departament = "DEV";
+		System.out.println(person3.nume);
+		System.out.println(person3.departament);
+		System.out.println("---------------------");
+		System.out.println(person.nume + person.departament);
+		System.out.println(person2.nume + person2.departament);
+		System.out.println(person3.nume + person3.departament);
+	}
+
+}
