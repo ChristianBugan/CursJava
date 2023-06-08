67ad9c8e00ce74ca1313ffbbc55d1a74bbb20dfa
unknown <christian.bugan@gmail.com>
2023-06-08 10:08:38 +0300

    curs 8 intermediar

---
+++ src/curs8/Deer.java
@@ -0,0 +1,14 @@
+package curs8;
+
+public class Deer extends WildAnimal {
+    
+	@Override
+	public void makeSound() {
+		System.out.println("Meeeeeh !");
+	}
+	
+	public void eatGrass() {
+		System.out.println("I eat grass and I like it!");
+	}
+	
+}
---
+++ src/curs8/Lion.java
@@ -0,0 +1,15 @@
+package curs8;
+
+public class Lion extends WildAnimal {
+
+	
+	public void makeSound() {
+		System.out.println(" Roaaaaaarrrh !");
+		
+	}
+	
+	public void eatMeat() {
+		System.out.println("I eat meat!");
+	}
+	
+}
